bd6622a0d19fdd43d10b837febdfb1a4175dc2c0
IsraelVelizHerrera <israel_razer@hotmail.com>
2024-11-12 19:03:14 +0100

    creacion de migraciones y modelos

---
+++ database/migrations/2024_11_06_145113_create_contactos_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('contactos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre', 50);
+            $table->string('celular', 15);
+            $table->string('servicio', 20);
+            $table->string('bloque', 10)->nullable();
+            $table->string('email', 50)->nullable();
+            $table->boolean('estado')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('contactos');
+    }
+};
